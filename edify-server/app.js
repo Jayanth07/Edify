4c256a6f39d69a3bb48918e3a5c82b7a07cc1989
Jayanth07 <jayanth.saisurya@gmail.com>
2022-05-01 16:14:48 -0500

    apis for users collection

--- edify-server/app.js
+++ edify-server/app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 var tutorsRouter = require('./routes/tutors');
 var appointmentsRouter = require('./routes/appointments');
 
@@ -21,6 +22,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/users', usersRouter);
 app.use('/tutors', tutorsRouter);
 app.use('/appointments', appointmentsRouter);
 
--- edify-server/routes/users.js
+++ edify-server/routes/users.js
@@ -1,9 +1,79 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+var monk = require('monk')
+var db = monk('localhost:27017/edify');
+
+var collection = db.get('users');
+
+router.get('/', function(req, res) {
+  collection.find({}, function(err, tutors) {
+    if (err)
+     throw err;
+    res.json(tutors);
+  });
 });
+
+router.post('/', function(req, res) {
+
+  const { email, password } = req.body;
+
+  if (!(email && password)) {
+    res.send('All fields are required!')
+    res.status(400);
+  } else {
+    collection.find({email: email}, function(err, tutor) {
+      if (err)
+        throw err;
+  
+      if (tutor) {
+        res.send('An account for this email already exists, please login!')
+        res.status(400);
+      } else {
+        collection.insert({
+            email,
+            password: hash(password) // Password should be hashed in the UI and sent via network
+        }, function(err, user) {
+          if (err)
+           throw err;
+          res.json(user);
+        });
+      }
+    })
+  }  
+  });
+
+
+  router.put('/:id', function(req, res) {
+    
+  const { email, password } = req.body;
+
+  if (!(email && password)) {
+    res.send('All fields are required!')
+    res.status(400)
+  } else {
+    collection.update({
+        email: email
+    }, {
+        $set: {
+          email: email,
+          password: password
+        }
+    }, function(err, user) {
+      if (err)
+       throw err;
+      res.json(user);
+    });
+  }
+  });
+
+  router.delete('/:id', function(req, res) {
+    collection.remove({ _id: req.params.id }, function(err, user) {
+      if (err)
+       throw err;
+      res.json(user);
+    });
+  });
+  
 
 module.exports = router;
